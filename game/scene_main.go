fa6c020b479898ee8670d31294b5c7bbc760b3b1
ichibankunio <kunio.ichiban@gmail.com>
2026-02-21 23:27:10 +0900

    feat: resolve issue #12

--- game/scene_main.go
+++ game/scene_main.go
@@ -60,7 +60,7 @@ func (s *MainScene) Draw(screen *ebiten.Image) {
 	if s.font != nil {
 		op := &text.DrawOptions{}
 		op.GeoM.Translate(40, 610)
-		text.Draw(screen, "Hi World", s.font, op)
+		text.Draw(screen, "Hello World", s.font, op)
 	}
 }
 
